b2648d94800768503b1561df1e349801bba8e2ce
Ahmed Chouihi <ahmedchouihi@gmail.com>
2024-02-13 23:42:34 +0100

    Views in Symfony

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,19 +2,18 @@
 
 namespace App\Controller;
 
-use http\Env\Response;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies/{name}', name: 'movies', defaults: ['name'=>null],methods: ['GET','HEAD'])]
-    public function index($name): JsonResponse
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => $name,
-            'path' => 'src/Controller/MoviesController.php',
+        return $this->render('index.html.twig',[
+            'title'=>''
         ]);
     }
 }
