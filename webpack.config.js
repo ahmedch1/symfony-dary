78d5cb98986877a3f6ba7dcedf7253f436af2139
Ahmed Chouihi <ahmedchouihi@gmail.com>
2024-02-18 21:06:23 +0100

    how to install tailwind3 in symfony 6

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,11 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+    .enablePostCssLoader((options)=>{
+        options.postcssOptions={
+            config:'./postcss.config.js'
+        }
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
